e54212ecf3711c07d4762674d8693872c5c8f7f2
okothpius <orukopius@gmail.com>
2021-01-13 18:18:37 +0300

    add serializers

--- leads/serializers.py
+++ leads/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from .models import Lead
 
 class LeadSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = Lead
         fields = ('id', 'name', 'email', 'message')
