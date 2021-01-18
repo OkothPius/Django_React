8fda0358648ab36285fc3eb8580ec5494ddf771c
okothpius <orukopius@gmail.com>
2021-01-18 12:34:25 +0300

    disable the browsable api in prod

--- django_react/settings.py
+++ django_react/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'django_react.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_RENDERER_CLASSES': (
-#         'rest_framework.renderers.JSONRenderer',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
 
 DATABASES = {
     'default': {
